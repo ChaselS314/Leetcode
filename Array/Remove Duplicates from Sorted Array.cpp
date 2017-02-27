84b2efde99049e995f4f27f96f4134910f78448b
Chen Su <420703660@qq.com>
2017-02-27 20:21:02 +0800

    add it

---
+++ Array/Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i = 0, j = 1;
+        if(nums.size() < 2) return nums.size();
+        while(j < nums.size()){
+            if(nums[i] != nums[j]){
+                nums[++i] = nums[j];
+            }
+            j++;
+        }
+        return i+1;
+    }
+};
