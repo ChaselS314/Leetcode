7d0cec28369c738e582c9a3eb8847479fe1a61ea
int_boy <intboy.1423@gmail.com>
2017-10-09 19:50:11 +0800

    update

--- Find the Duplicate Number.cpp
+++ Find the Duplicate Number.cpp
@@ -1,3 +1,4 @@
+// First solution : sort and traversal
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
@@ -5,5 +6,22 @@ public:
         for(auto it = nums.begin() + 1; it != nums.end(); it++){
             if(*it == *(it-1)) return *it;
         }
+    }
+};
+// Second solution : quick and slow pointer(better)
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int fast = nums[nums[0]], slow = nums[0];
+        while(fast != slow){
+            fast = nums[nums[fast]];
+            slow = nums[slow];
+        }
+        fast = 0;
+        while(fast != slow){
+            fast = nums[fast];
+            slow = nums[slow];
+        }
+        return fast;
     }
 };
